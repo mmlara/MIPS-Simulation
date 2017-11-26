4ca50a245d06db64f051059c8eb532c14d8ae15f
bgd95 <bjgd95@gmail.com>
2017-11-26 14:49:35 -0600

    Clock Doc

--- src/physicalcomponentssimulation/time/Clock.java
+++ src/physicalcomponentssimulation/time/Clock.java
@@ -1,16 +1,37 @@
+/**
+ *  Clock
+ *
+ * @author Gómez Brayan, Lara Milton, Quirós Esteban
+ * @version 1.0
+ * @since 25/11/2017
+ */
 package physicalcomponentssimulation.time;
 
 public class Clock {
 
+    /**
+     * Current cycle in the simulation
+     */
     private int currentTime;
 
+    /**
+     * constructor
+     */
     public Clock(){
         this.currentTime=0;
     }
+
+    /**
+     * get Current time
+     * @return current cycle
+     */
     public int getCurrentTime() {
         return currentTime;
     }
 
+    /**
+     * Increase in one cycle the clock
+     */
     public void increaseCurrentTime() {
         this.currentTime++;
     }
